b2a8c4deb9eeb0af5ed29a9174f10a7ed9f6338b
Mohamed Fouad Kanoun <foued.kanoun@openinnovation.ai>
2024-02-17 19:28:58 +0400

    feat: config and create client api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
+import ApiClient from "api/ApiClient";
 import AddButton from "components/Button/AddButton";
-import logo from "./logo.svg";
-import "./styles/app.css";
+import axiosInstance from "config/axios.config";
+import { User } from "models/user.model";
+import { useEffect } from "react";
+
+const api = new ApiClient<User>(axiosInstance);
 
 function App() {
+  useEffect(() => {
+    (async function name() {
+      const { data } = await api.get("/users?size=10");
+      console.log(data);
+    })();
+  }, []);
+
   return (
-    <div className="App">
+    <div>
       <AddButton />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="p-8"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Learn React</header>
     </div>
   );
 }
