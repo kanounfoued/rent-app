9002c6763731fe3da8c813b21ad28447b8e858cc
Mohamed Fouad Kanoun <foued.kanoun@openinnovation.ai>
2024-08-20 21:19:26 +0400

    feat: update entities model from type to interface

--- src/models/entities.model.ts
+++ src/models/entities.model.ts
@@ -2,3 +2,12 @@ export enum ENTITIES {
   PROPERTY = 'property',
   USER = 'user',
 }
+
+export interface Entity {
+  id?: string;
+}
+
+export interface Meta {
+  created_at: string;
+  updated_at: string;
+}
